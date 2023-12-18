5c83704f66a34e8c70bb29d2bfb79bd463575cff
SovetSamat <iretoviret@gmail.com>
2023-12-18 14:54:49 +0300

    Color changes, added session record, added infinty scrreen

--- colors.py
+++ colors.py
@@ -1,6 +1,9 @@
-Black = (0,0,0)
-White = (255, 255, 255)
-YellowGreen = (154, 205, 50)
-SkyBlue = (135, 206, 235)
-BurlyWood = (222, 184, 135)
-DarkGray = (169, 169, 169)
+Black = 0, 0, 0
+White = 255, 255, 255
+Crimson = 220, 20, 60
+YellowGreen = 154, 205, 50
+ForestGreen	= 34, 139, 34
+SkyBlue = 135, 206, 235
+BurlyWood = 222, 184, 135
+DarkGray = 169, 169, 169
+DimGrey = 105, 105, 105
